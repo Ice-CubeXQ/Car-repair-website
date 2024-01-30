7ecd433401594ca37b2f8c62b4844f94004c365c
Ice-CubeXQ <dealplug72@gmail.com>
2024-01-30 22:12:47 +0300

    style for header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,10 @@
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
+if (document.querySelector(".search__icon")) {
+  const searchButton = document.querySelector(".search__icon");
+  const searchBlock = document.querySelector(".search");
+  searchButton.addEventListener("click", function (e) {
+    searchBlock.classList.toggle("_active");
+  });
+}
