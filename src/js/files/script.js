f6dfe6d1d1feb2cc469e9e20fb18d8113f318816
Ice-CubeXQ <dealplug72@gmail.com>
2024-02-01 19:04:56 +0300

    adaptive header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,24 +2,6 @@
 import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
-// if (document.querySelector(".search__icon")) {
-//   const searchButton = document.querySelector(".search__icon");
-//   const searchBlock = document.querySelector(".search");
-//   const searchResultList = document.querySelector(".search__results-list");
-//   searchButton.addEventListener("click", function (e) {
-//     searchBlock.classList.toggle("_active");
-//     if (searchBlock.classList.contains("_active")) {
-//       setTimeout(function () {
-//         searchBlock.style.overflow = "visible";
-//       }, 300);
-
-//       searchResultList.style.cssText = `display: none;`;
-//     } else {
-//       searchBlock.style.overflow = "hidden";
-//     }
-//   });
-// }
-
 if (isMobile.any()) {
   document.addEventListener("click", function (e) {
     if (e.target.classList.contains("menu__arrow")) {
@@ -30,44 +12,6 @@ if (isMobile.any()) {
     }
   });
 }
-
-// document.querySelector("#search-line").oninput = function () {
-//   let value = this.value.trim().toLowerCase();
-//   const searchResultList = document.querySelector(".search__results-list");
-//   searchResultList.style.cssText = `display: none;`;
-//   const items = {
-//     oilChange: {
-//       link: "https://example.com/oil-change",
-//     },
-//     brakeReplacement: {
-//       link: "https://example.com/brakeReplacement",
-//     },
-//     tireRotation: {
-//       link: "https://example.com/tireRotation",
-//     },
-//     engineTuneUp: {
-//       link: "https://example.com/engineTuneUp",
-//     },
-//   };
-//   let searchResultsHTML = "";
-//   let valueInput = false;
-//   if (value != "") {
-//     searchResultList.style.cssText = `display: grid;`;
-//     valueInput = true;
-//     for (let key in items) {
-//       let formattedKey = key.replace(/([A-Z])/g, " $1").trim();
-
-//       if (formattedKey.toLowerCase().includes(value)) {
-//         searchResultsHTML += `<li class="search__item"><a href="${items[key].link}" class="search__link">${formattedKey}</a></li>`;
-//       }
-//     }
-//   }
-
-//   if (searchResultsHTML === "" && valueInput) {
-//     searchResultsHTML = `<li class="search__item"><p class="search__link">Nothing found</p></li>`;
-//   }
-//   searchResultList.innerHTML = searchResultsHTML;
-// };
 
 const searchButton = document.querySelector(".search__icon");
 const searchLine = document.querySelector("#search-line");
